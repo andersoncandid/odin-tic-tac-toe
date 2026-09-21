e79852a64f320179978916314475a5804565a77b
anderson.candido <190989879+andersoncandid@users.noreply.github.com>
2026-09-21 02:16:29 -0300

    update game logic

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 // --- Gameboard ---
 // Create and update the state of the board
 const gameboard = (() => {
-  let board = [];
-  let columns = 3;
-  let rows = 3;
-
-  // Creating two-dimensional board array
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i][j] = "";
-    }
-  }
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]; // Tic tac toe have a fixed size board
 
   const getBoard = () => board;
 
@@ -28,7 +22,7 @@ const gameboard = (() => {
 function player(name, mark) {
   let wins = 0;
   let matchesPlayed = 0;
-  let score = [wins, matchesPlayed];
+  const score = [wins, matchesPlayed];
 
   // Track the player`s score
   const updateScore = (win) => {
@@ -42,10 +36,52 @@ function player(name, mark) {
   return { name, mark, updateScore, getScore };
 }
 
-// --- Game ---
+// --- Game controler ---
 // Manage game assets and control the flow
+const game = ((playerOneName, playerTwoName) => {
+  const playerOne = player(playerOneName, "x");
+  const playerTwo = player(playerTwoName, "o");
+  let nextPlayerTurn = 1;
 
-// const game = ((playerOneName, playerTwoName) => {
-// // Initialize the game, and create game resources
-//
-// })();
+  const getNextPlayer = () => nextPlayerTurn;
+
+  // Control of game actions
+  const takePlayerTurn = (row, column, player) => {
+    gameboard.updateBoard(row, column, player.mark);
+
+    // Change the active player order
+    if (nextPlayerTurn === 1) {
+      nextPlayerTurn = 2;
+    } else {
+      nextPlayerTurn = 1;
+    }
+  };
+
+  // Check if win condition is fulfilled
+  const checkForWinner = () => {
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        let cell = board[row][col];
+        let completeRow = true;
+        let completeCol = true;
+        let completeDiag1 = true;
+        let completeDiag2 = true;
+
+        // check for any complete diagonals
+        if (row === col && cell != bord[0][0]) {
+          completeDiag = false;
+        }
+
+        // check for any complete row
+        if (cell != board[row][0]) {
+          completeRow = false;
+        }
+        // check for any complete column
+        if (cell != board[0][col]) {
+          completeCol = false;
+        }
+        return cell;
+      }
+    }
+  };
+})();
